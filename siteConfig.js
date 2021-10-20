234146659176f116130fcd53623206646a453b63
juruketik12 <juruketik12@gmail.com>
2021-10-20 18:57:12 +0700

    add build

--- siteConfig.js
+++ siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://juruketik12.github.io', // Your website URL
-  baseUrl: '/docusaurus_learn/', // Base URL for your project */
+  baseUrl: '/docusaurus-learn/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docusaurus-tutorial',  // The name of your GitHub project. Same as above.
+  projectName: 'docusaurus-learn',  // The name of your GitHub project. Same as above.
   organizationName: 'juruketik12',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
